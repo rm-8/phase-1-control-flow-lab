56198b190d5c57e6e7fe1dec9ebcd2bafbbb97af
rm-8 <mohamedridhwan8@gmail.com>
2023-12-11 23:13:21 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function scuberGreetingForFeet(){
-  // Write your code here!
+  if (ride <= 400) {
+    return "This one is on me!";
+
+  }else if (ride > 2000 && ride < 2500) {
+    return "I will gladly take your thirty bucks.";
+  }else if (ride > 2500) {
+    return "No can do.";
+  }
 }
 
 function ternaryCheckCity(){
